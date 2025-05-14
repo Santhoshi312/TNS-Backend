80aa4c376a064e2866a4547336b74b95a025fa7b
sanso <sanso@LAPTOP-5518V52B>
2025-05-14 21:17:13 +0530

    Types of Variables

---
+++ TNS/src/com/tnsif/VarDemo/Student.java
@@ -0,0 +1,10 @@
+package com.tnsif.VarDemo;
+
+public class Student {
+	int sid;
+	String sname;
+	static String collegename="Sri Indu";
+	 void display() {
+		 System.out.println("The details of the student"+sid+","+sname+","+collegename);
+	 }
+}
---
+++ TNS/src/com/tnsif/VarDemo/TypesVarDemo.java
@@ -0,0 +1,14 @@
+package com.tnsif.VarDemo;
+
+public class TypesVarDemo {
+	//instance variable
+			int numone;
+			//method
+			void display() {
+				//local variable
+			     int numtwo=20;
+				System.out.println("This is local variable "+numtwo);
+			}
+			//static variable
+			static int numthree=30;
+}
